5a197284db62255efbe30820fa507dc70f0bc34d
Cdk29 <etienne.rolland.cdk@gmail.com>
2023-02-17 10:16:17 +0100

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,40 +1,22 @@
+import selenium
+import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+class NewVisitorTest(unittest.TestCase):
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
- 
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-for option in options:
-    chrome_options.add_argument(option)
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_that_the_website_exist(self):
+        self.browser.get('https://www.projet-ipf2023.com/')
+        title = self.browser.title
+        self.assertIn('', title)
 
-    
-driver = webdriver.Chrome(options = chrome_options)
-
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+unittest.main()
 
