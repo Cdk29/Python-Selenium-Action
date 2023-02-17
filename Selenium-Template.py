6f3e2725d277dec367e7889611f72b1249188cd3
Cdk29 <etienne.rolland.cdk@gmail.com>
2023-02-17 10:29:06 +0100

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -2,6 +2,7 @@ import selenium
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import FirefoxOptions
 import time
 import unittest
 
